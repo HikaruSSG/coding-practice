353edcfdceb2f98bf3ac830ff4642c040e1bf58b
HikaruSSG <5PB25QbOcJQnUWT_h0o9O6tOkN5CsuU@protonmail.com>
2024-12-21 12:41:20 +0800

    enhance config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,34 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import pluginImport from "eslint-plugin-import";
+import pluginNode from "eslint-plugin-node";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.browser,
+      },
+    },
+    env: {
+      es2021: true,
+      node: true,
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    plugins: {
+      import: pluginImport,
+      node: pluginNode,
+    },
+    rules: {
+      "import/no-unresolved": "error",
+      "node/no-missing-import": "error",
+      "node/no-unpublished-import": "error",
+      "node/no-unsupported-features/es-syntax": "off",
+    },
+  },
 ];
